51b3abdfb9bfc866d651dca7ee51f9ced435e49d
hensal <hensaldeon777@gmail.com>
2023-05-11 09:51:58 +0530

    done

---
+++ 434. Number of Segments in a String.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var countSegments = function(s) {
+    let length;
+    if (s.trim() === "") {
+        return 0;
+    }
+    const segments = s.trim().split(/\s+/);
+    return length = segments.length;
+    
+};
