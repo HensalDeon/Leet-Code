ff0299ebabeaa5ac37ce549626e87d9dbbd21fe3
hensal <hensaldeon777@gmail.com>
2023-06-15 09:51:01 +0530

    commit

---
+++ 219. Contains Duplicate II.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function (nums, k) {
+    for (let i = 0; i < nums.length - 1; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] == nums[j] && Math.abs(i - j) <= k) {
+                return true;
+            }
+        }
+    }
+    return false;
+};
