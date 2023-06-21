e690a6def1984ebdc5dabf3647dbbaa8c9e76dee
hensal <hensaldeon777@gmail.com>
2023-06-21 09:37:10 +0530

    commit

---
+++ 1342. Number of Steps to Reduce a Number to Zero.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var numberOfSteps = function (num) {
+    let step = 0;
+    while (num > 0) {
+        num = num % 2 === 0 ? num / 2 : num-1
+        step++;
+    }
+    return step;
+};
