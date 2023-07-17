74318a65d12f256c43c5b0ae5659743cfc5affa2
hensal <hensaldeon777@gmail.com>
2023-07-18 01:21:39 +0530

    update

---
+++ 118. Pascal's Triangle.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate = function (numRows) {
+    let arr = [];
+
+    for (let i = 0; i < numRows; i++) {
+        arr.push(Array(i + 1))
+        for (let j = 0; j <= i; j++) {
+            if (j === 0 || j === i) {
+                arr[i][j] = 1
+            } else {
+                arr[i][j] = arr[i - 1][j - 1] + arr[i - 1][j]
+            }
+        }
+    }
+    return arr
+};
